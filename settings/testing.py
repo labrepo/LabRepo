f14fca609205d697c00cceb64f01d540daf642bc
cyc60 <avsysoev60@gmail.com>
2015-10-21 17:27:07 +0300

    Fix test runner

--- settings/testing.py
+++ settings/testing.py
@@ -1,4 +1,8 @@
 from base import *
+from os import environ
+
+import dotenv
+dotenv.read_dotenv()
 
 TESTING = os.sys.argv[1:2] == ['test']
 
@@ -8,12 +12,12 @@ DOMAIN = '0.0.0.0:8000'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 
-_MONGODB_NAME = 'scincelab_test'
-_MONGODB_HOST = '127.0.0.1'
-_MONGODB_PORT = 27017
-_MONGODB_USER = 'admin'
-_MONGODB_PASSWD = 'password'
-mongoengine.connect(_MONGODB_NAME, tz_aware=USE_TZ)
+_MONGODB_NAME = environ.get('MONGODB_TEST_DATABASE')
+
+_MONGODB_USER = environ.get('MONGODB_USER')
+_MONGODB_PASSWD = environ.get('MONGODB_PASSWD')
+_MONGODB_HOST = environ.get('MONGODB_HOST')
+_MONGODB_PORT = environ.get('MONGODB_PORT')
 
 # TEST_RUNNER = 'django_selenium.selenium_runner.SeleniumTestRunner'
 # SELENIUM_DRIVER = 'Firefox'
@@ -22,6 +26,8 @@ mongoengine.connect(_MONGODB_NAME, tz_aware=USE_TZ)
 _MONGODB_DATABASE_HOST = \
     'mongodb://%s:%s@%s:%s/%s' \
     % (_MONGODB_USER, _MONGODB_PASSWD, _MONGODB_HOST, _MONGODB_PORT, _MONGODB_NAME)
+
+mongoengine.connect(_MONGODB_NAME, host=_MONGODB_DATABASE_HOST, tz_aware=USE_TZ)
 
 BROKER_URL = _MONGODB_DATABASE_HOST
 
