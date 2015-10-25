279dbf1968ecbb56b6727c41d552f82c462ec55a
cyc60 <avsysoev60@gmail.com>
2015-10-26 00:51:13 +0300

    Move a static folder from common to the root.

--- settings/base.py
+++ settings/base.py
@@ -148,6 +148,10 @@ DATABASES = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static/"),
+)
+
 STATIC_URL = '/static/'
 
 STATIC_ROOT = rel('public', 'static')
