0e08bc7638801ce7b32bf3c2c00885ce95a9e082
cyc60 <avsysoev60@gmail.com>
2015-11-02 16:01:21 +0300

    Rename testing file for not triggering test. Update test celery settings

--- settings/server_testing.py
+++ settings/server_testing.py
@@ -29,27 +29,14 @@ _MONGODB_DATABASE_HOST = \
 
 mongoengine.connect(_MONGODB_NAME, host=_MONGODB_DATABASE_HOST, tz_aware=USE_TZ)
 
-BROKER_URL = _MONGODB_DATABASE_HOST
 
-CELERY_MONGODB_BACKEND_SETTINGS = {
-    "host": _MONGODB_HOST,
-    "port": _MONGODB_PORT,
-    'user': _MONGODB_USER,
-    'password': _MONGODB_PASSWD,
-    'database': _MONGODB_NAME,
-    'taskmeta_collection': 'celery_tasks_test',
-}
+BROKER_URL = 'redis://localhost:6379/10'
+
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/10'
 CELERY_ACCEPT_CONTENT = ['pickle']
 CELERY_EVENT_SERIALIZER = 'pickle'
-CELERY_RESULT_BACKEND = "mongodb"
 
-BROKER_BACKEND = "mongodb"
-BROKER_HOST = _MONGODB_HOST
-BROKER_PORT = _MONGODB_PORT
-BROKER_USER = _MONGODB_USER
-BROKER_PASSWORD = _MONGODB_PASSWD
-BROKER_VHOST = "celery"
-
+ES_DISABLED = True
 
 if TESTING and 'test' not in _MONGODB_NAME:
     raise SystemExit('Wrong mongo db?')
